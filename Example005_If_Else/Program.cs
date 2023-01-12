82ac0fde3c32c4ae799a13b3465dd19d7d862ae8
Dmitry <adachel74@gmail.com>
2023-01-13 00:18:27 +0500

    Example005 If else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "дмитрий")
+{
+    Console.WriteLine("Приветствую Вас дорогой Дмитрий");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
