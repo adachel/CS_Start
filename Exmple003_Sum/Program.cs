ef54969a2ff9bcaed7a4e50edda80124c42f7cd2
Dmitry <adachel74@gmail.com>
2023-01-12 23:46:55 +0500

    Raandom

--- Exmple003_Sum/Program.cs
+++ Exmple003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int NumberA = 3;
-int NumberB = 5;
+﻿int NumberA = new Random().Next(1, 10); // Задает числа от 1 до 9(max-1)
+Console.WriteLine(NumberA);
+int NumberB = new Random().Next(1, 10);
+Console.WriteLine(NumberB);
 int result = NumberA + NumberB;
 Console.WriteLine(result);
