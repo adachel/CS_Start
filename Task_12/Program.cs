efd853c760e8f8593ca0561c7d89a782c7411377
Dmitry <adachel74@gmail.com>
2023-01-19 18:03:29 +0500

    Task_12 +1

--- Task_12/Program.cs
+++ Task_12/Program.cs
@@ -5,15 +5,20 @@ Console.Clear();
 Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
-if (a / 100 < 1)
+if (a > -100  && a < 100)
 {
     Console.WriteLine("Третьей цифры нет"); 
     return;
 }
 
+if (a <= -100)
+{
+    a = a * (-1);
+}
+
 for (int i = 10; a >= 1000; i = i * 1)
     a = a / i;
 
-    int b = a % 10;
-
-Console.WriteLine($"{b}"); 
+int b = a % 10;
+    
+Console.WriteLine($"Третья цифра числа: {b}"); 
