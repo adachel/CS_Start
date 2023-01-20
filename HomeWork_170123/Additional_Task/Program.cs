cfac1b8d69541b6373a5e80311dd9a67c9cd49af
Dmitry <adachel74@gmail.com>
2023-01-20 21:54:07 +0500

    Код препода Task_Add

--- HomeWork_170123/Additional_Task/Program.cs
+++ HomeWork_170123/Additional_Task/Program.cs
@@ -26,3 +26,25 @@ while (a != 0)
 Console.WriteLine($"Второе наибольшее число: {max2}"); 
 
 
+/*
+Console.Clear();
+int a = Convert.ToInt32(Console.ReadLine()), max1 = a, max2 = 0;
+
+while (a != 0)
+{
+    a = Convert.ToInt32(Console.ReadLine());
+    if (a > max1)
+    {
+        max2 = max1;
+        max1 = a;        
+    }
+
+    else if (a >= max2)
+    {
+        max2 = a;
+    }
+}
+
+Console.WriteLine($"Второе наибольшее число: {max2}"); 
+
+*/
