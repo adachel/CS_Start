f17a9f8df1017672cb18e2b7c8181c527eb854be
Dmitry <adachel74@gmail.com>
2023-01-20 21:43:10 +0500

    Код препода Task_13

--- HomeWork_170123/Task_13/Program.cs
+++ HomeWork_170123/Task_13/Program.cs
@@ -17,3 +17,28 @@ for (int i = 10; a >= 1000; i = i * 1)
 int b = a % 10;
     
 Console.WriteLine($"Третья цифра числа: {b}"); 
+
+/*
+// Код препода
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 100)
+    Console.WriteLine("Третьей цифры нет"); 
+
+else
+{
+    while (a >= 1000)
+    {
+        a = a / 10;
+    }
+    Console.WriteLine($"Число: {a}"); 
+    Console.WriteLine($"Третья цифра числа: {a % 10}"); 
+
+}
+*/
+
+
+
+
