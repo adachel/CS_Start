0052c2142300349b8a49ea797d49cb2411aab996
Dmitry <adachel74@gmail.com>
2023-01-28 05:55:57 +0500

    Seminar_270123

--- HomeWork_240123/Task_25/Program.cs
+++ HomeWork_240123/Task_25/Program.cs
@@ -16,3 +16,17 @@ Console.WriteLine();
 
 //double b = Math.Pow(n, a); // Для проверки
 //Console.WriteLine($"Проверка {n} в степени {a} равно {b} ");
+
+
+/*
+// task 25
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine()), result = 1;
+for (int i = 1; i <= m; i++)
+    result = result * n; // result *= n;
+Console.WriteLine(result);
+*/
+
--- HomeWork_240123/Task_27/Program.cs
+++ HomeWork_240123/Task_27/Program.cs
@@ -13,3 +13,16 @@ for (int i = 1; n > 0; i++)
 
 Console.WriteLine($"Cуммa цифр в числе: {sum} ");
 
+
+/*
+// task 27
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), result = 0;
+while (n > 0)
+{
+    result = result + n % 10; // result += n %  10;
+    n = n / 10; // n /= 10;
+}
+Console.WriteLine(result);
+*/
