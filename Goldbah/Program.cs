7c6aa63c83f0b38faa37e90a305f96adaa743440
Dmitry <adachel74@gmail.com>
2023-01-31 00:58:21 +0500

    Убрал гольбха

--- Goldbah/Program.cs
+++ Goldbah/Program.cs
@@ -2,28 +2,44 @@
 // разложение заданного чётного числа. Из всех пар простых чисел, 
 // сумма которых равна заданному числу, требуется найти пару, содержащую наименьшее простое число.
 
+
 int SimpleNumber (int n) 
 {
-    int a = 0; // Составное число
+    int a = 2;
     int b = 0; // Простое число
-    if (n == 2 || n == 3 || n == 5)
-        b = n;  
-    
-    else if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
-        a = n;
-        
-    else
+    if (n == 2)
         b = n;
 
+    while (a < n) 
+    {
+        if (n%a == 0)
+        {
+            Console.WriteLine($"{n%a} - хрень");
+            break;
+        }
+        
+        if (n%a != 0)
+            {
+                b = n;
+                Console.WriteLine($"{b} - rty");
+            }    
+
+        a = a + 1;
+
+    }
     return b;
 }
 
-Console.Clear();
-Console.Write("Введите четное число больше <2>: ");
-int Number = Convert.ToInt32(Console.ReadLine());
-int dif = 0;
-int n = 2;
 
+Console.Clear();
+//Console.Write("Введите четное число больше <2>: ");
+//int Number = Convert.ToInt32(Console.ReadLine());
+//int dif = 0;
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{SimpleNumber (n)} - первое число");
+
+
+/*
 for (int i = 2; i <= Number / 2; i++)
 {
     if (SimpleNumber (n) > 0)
@@ -39,6 +55,7 @@ for (int i = 2; i <= Number / 2; i++)
     
     n = n + 1;
 }
+*/
 
 
 
