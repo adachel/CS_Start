84365e79259d60cc9a9ad34e84499dcce4689f48
Dmitry <adachel74@gmail.com>
2023-02-09 21:33:31 +0500

    Сделал 62

---
+++ Tasks62/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+
+void ScreenMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int size = 4;
+int[,] matrix = new int[size, size];
+
+int count = 1;
+int i = 0;
+int j = 0;
+while(count <= 4 * 4)
+{
+    matrix[i, j] = count;
+    count++;
+    if(i <= j + 1 && i + j < size - 1) j++;
+    else if(i < j && i + j >= size - 1) i++;
+    else if(i >= j && i + j >= size) j--;
+    else if(i > j + 1 && i + j < size) i--;
+}
+ScreenMatrix(matrix);
