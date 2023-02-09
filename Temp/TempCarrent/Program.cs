519b88a4275c6252ae9b50735da364e4946b00ca
Dmitry <adachel74@gmail.com>
2023-02-09 06:37:58 +0500

    изм

--- Temp/TempCarrent/Program.cs
+++ Temp/TempCarrent/Program.cs
@@ -1,47 +1,52 @@
 ﻿// Задача 62: Заполните спирально массив 4 на 4.
-// 1 2 3 4
+//  1  2  3 4
 // 12 13 14 5
 // 11 16 15 6
-// 10 9 8 7
+// 10  9  8 7
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] / 10 <= 0)
+                Console.Write($" {array[i, j]} ");
+
+            else Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
 
 Console.Clear();
-Console.WriteLine($"Задача 62: Заполните спирально массив 4 на 4.");
-
 int n = 4;
-int[,] sqareMatrix = new int[n, n];
+int[,] Matrix = new int[n, n];
+
+//Console.WriteLine($" {Matrix[1, 2]} ");
 
 int temp = 1;
 int i = 0;
 int j = 0;
 
-while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+while (temp <= Matrix.GetLength(0) * Matrix.GetLength(1))
 {
-  sqareMatrix[i, j] = temp;
-  temp++;
-  if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
-    j++;
-  else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
-    i++;
-  else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
-    j--;
-  else
-    i--;
+    Matrix[i, j] = temp;
+    temp++;
+
+    Console.Write($" {Matrix[0, 0]} ");
+    
+    // if (i <= j + 1 && i + j < Matrix.GetLength(1) - 1)
+    //     j++;
+    // else if (i < j && i + j >= Matrix.GetLength(0) - 1)
+    //     i++;
+    // else if (i >= j && i + j > Matrix.GetLength(1) - 1)
+    //     j--;
+    // else
+    //     i--;
 }
 
-WriteArray(sqareMatrix);
+//WriteArray(Matrix);
 
-void WriteArray (int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      if (array[i,j] / 10 <= 0)
-      Console.Write($" {array[i,j]} ");
 
-      else Console.Write($"{array[i,j]} ");
-    }
-    Console.WriteLine();
-  }
-}
 
