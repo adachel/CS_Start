2a84864132ad2010cd415ce4c2fb1fd3be48b60a
Dmitry <adachel74@gmail.com>
2023-02-20 14:53:45 +0500

    обединение

---
+++ Forms/Объеденить_цифры/Program.cs
@@ -0,0 +1,9 @@
+﻿// Объеденить две цифры в одну
+
+Console.Clear();
+Console.Write("Ведите два чиасла через пробел: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int a = size[0];
+int b = size[1];
+int c = int.Parse(a.ToString() + b.ToString());
+Console.Write($"Итоговое число: {c}");
