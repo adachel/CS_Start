28ab82a83d398841b5f1261f51acf5816c7f9cc0
Dmitry <adachel74@gmail.com>
2023-02-27 12:30:43 +0500

    qwer

--- BootCamp/ЭффективностАлгоритмовЗадача1/Program.cs
+++ BootCamp/ЭффективностАлгоритмовЗадача1/Program.cs
@@ -9,11 +9,11 @@
 // }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
-// int GetValueByUser(string text) // Метод ввода числа с клавиатуры без проверки на валидность.
-// {
-//     Console.Write(text);
-//     return int.Parse(Console.ReadLine()!); // Parse  - преобразует строку в число
-// }
+int GetValueByUser(string text) // Метод ввода числа с клавиатуры без проверки на валидность.
+{
+    Console.Write(text);
+    return int.Parse(Console.ReadLine()!); // Parse  - преобразует строку в число
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
@@ -30,48 +30,48 @@
 
 /////////////////////////////////////////////////////////////////////////////////////
 
-// string PrintNumbers2(int n) // Корректный метод вывлда
-// {
-//   string output = String.Empty;
-//   for (int i = -n; i <= n; i++) // Плохая эффективность, нужно сократить кол-во итераций
-//   {
-//     //output = output + (i + "");
-//     output = output + $"{i} ";
-//   }
-//   return output;
-// }
-
-// int n = GetValueByUser("Введите число N: "); // Ввод числа
-// string res = PrintNumbers2(n);
-// Console.Write(res);
-//File.WriteAllText("data.txt", res); // Печать в файл
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-int GetValueByUser(string text) // Метод ввода числа с клавиатуры с проверки на валидность.
+string PrintNumbers2(int n) // Корректный метод вывoда
 {
-  int value = 0;
-  bool flag = false;
-  do
+  string output = String.Empty;
+  for (int i = -n; i <= n; i++) // Плохая эффективность, нужно сократить кол-во итераций
   {
-    Console.Write(text);
-    string s = Console.ReadLine()!;
-    flag = int.TryParse(s, out value) && value > 0;
-  } while (!flag);
-  return value;
-}
-
-string PrintNumbers3(int n) // Корректный метод вывлда
-{
-  string output = "0";
-
-  for (int i = 1; i <= n; i++) // По эффективности так лучше
-  {
-    output = $"{-i} " + output + $" {i}";
+    //output = output + (i + "");
+    output = output + $"{i} ";
   }
   return output;
 }
 
 int n = GetValueByUser("Введите число N: "); // Ввод числа
-string res = PrintNumbers3(n);
+string res = PrintNumbers2(n);
 Console.Write(res);
+//File.WriteAllText("data.txt", res); // Печать в файл
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// int GetValueByUser(string text) // Метод ввода числа с клавиатуры с проверки на валидность.
+// {
+//   int value = 0;
+//   bool flag = false;
+//   do
+//   {
+//     Console.Write(text);
+//     string s = Console.ReadLine()!;
+//     flag = int.TryParse(s, out value) && value > 0;
+//   } while (!flag);
+//   return value;
+// }
+
+// string PrintNumbers3(int n) // Корректный метод вывoда
+// {
+//   string output = "0";
+
+//   for (int i = 1; i <= n; i++) // По эффективности так лучше
+//   {
+//     output = $"{-i} " + output + $" {i}";
+//   }
+//   return output;
+// }
+
+// int n = GetValueByUser("Введите число N: "); // Ввод числа
+// string res = PrintNumbers3(n);
+// Console.Write(res);
